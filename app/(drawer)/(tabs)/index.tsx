b456763f88806d265c2eed7403ba22d94af55ba4
Markus Johansen <markusraunjohansen@gmail.com>
2024-03-19 16:29:44 +0100

    first commit

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function TabOneScreen() {
   return (
     <Theme name="light">
       <YStack flex={1} alignItems="center" justifyContent="center">
-        <H2>Tab One</H2>
+        <H2>Tab Uno</H2>
         <Separator />
         <EditScreenInfo path="app/(drawer)/(tabs)/index.tsx" />
       </YStack>
