9a2806d6358b18063b25bd4ee46473a784de6c89
Markus Johansen <markusraunjohansen@gmail.com>
2024-03-24 21:56:10 +0100

    Add settings and calender page

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,16 +1,25 @@
-import { FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
-import { Link } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 import { Drawer } from 'expo-router/drawer';
-import { Pressable, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const DrawerLayout = () => (
   <Drawer>
     <Drawer.Screen
       name="index"
       options={{
-        headerTitle: 'Home',
-        drawerLabel: 'Home',
+        headerTitle: 'Hjem',
+        drawerLabel: 'Hjem',
         drawerIcon: ({ size, color }) => <Ionicons name="home-outline" size={size} color={color} />,
+      }}
+    />
+    <Drawer.Screen
+      name="calender"
+      options={{
+        headerTitle: 'Kalender',
+        drawerLabel: 'Kalender',
+        drawerIcon: ({ size, color }) => (
+          <Ionicons name="calendar-outline" size={size} color={color} />
+        ),
       }}
     />
     <Drawer.Screen
@@ -19,7 +28,7 @@ const DrawerLayout = () => (
         headerTitle: 'Info',
         drawerLabel: 'Info',
         drawerIcon: ({ size, color }) => (
-          <Ionicons name="information-circle" size={size} color={color} />
+          <Ionicons name="information-circle-outline" size={size} color={color} />
         ),
       }}
     />
@@ -28,7 +37,17 @@ const DrawerLayout = () => (
       options={{
         headerTitle: 'Shop',
         drawerLabel: 'Shop',
-        drawerIcon: ({ size, color }) => <Ionicons name="cart" size={size} color={color} />,
+        drawerIcon: ({ size, color }) => <Ionicons name="cart-outline" size={size} color={color} />,
+      }}
+    />
+    <Drawer.Screen
+      name="settings"
+      options={{
+        headerTitle: 'Indstillinger',
+        drawerLabel: 'Indstillinger',
+        drawerIcon: ({ size, color }) => (
+          <Ionicons name="settings-outline" size={size} color={color} />
+        ),
       }}
     />
   </Drawer>
