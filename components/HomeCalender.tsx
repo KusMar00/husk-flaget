53b98e3405aec4dfe96757ae57e694c640a07d8b
Markus Johansen <markusraunjohansen@gmail.com>
2024-04-04 17:34:37 +0200

    Style home page

--- components/HomeCalender.tsx
+++ components/HomeCalender.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Circle, H2, XStack, View, Text, Separator, YStack, ScrollView } from 'tamagui';
 import { FlagDayType } from '~/utils';
 
@@ -8,33 +9,32 @@ interface HomeCalenderProps {
 
 const HomeCalender = ({ flagDays }: HomeCalenderProps) => {
   return (
-    <View>
-      <H2 p="$3" mb="$2">
+    <View p="$4">
+      <H2 mb="$2" h={'$6'}>
         <Text col="black">Kalender</Text>
       </H2>
-      <ScrollView p="$3" gap="$3">
+      <ScrollView>
         {flagDays.map((flagDay, index) => (
-          <View key={index} gap="$3">
+          <View key={index} mb="$4">
             <XStack alignItems="center" justifyContent="space-between" borderStyle={'solid'}>
               <View
                 flex={1}
                 flexDirection="row"
                 alignItems="center"
                 justifyContent="flex-start"
-                gap="$2">
-                <Circle bc="$blue7" size="$1" />
-                <Text fontSize={'$8'} fontWeight="400">
+                gap="$3">
+                <Circle bc="$red7" size="$1" />
+                <Text fontSize={'$6'} fontWeight="600">
                   {flagDay.title}
                 </Text>
               </View>
-              <View flexDirection="row" gap="$4">
-                <Text fontSize={'$6'} col="$gray10">
+              <View flexDirection="row" w={'20%'} justifyContent="flex-end">
+                <Text fontSize={'$4'} col="$gray10">
                   {flagDay.date.getDate()}/{flagDay.date.getMonth()}
                 </Text>
-                <Text fontSize={'$6'}>Officiel</Text>
               </View>
             </XStack>
-            <Separator borderColor={'$gray10'} />
+            <Separator borderColor={'$gray12'} mt="$4" />
           </View>
         ))}
       </ScrollView>
